14966fe9a0f4036ffd1e507e4fb556f076813d08
dhavvval <ajana.dhaval.2@gmail.com>
2026-02-20 14:19:44 -0600

    Added PrimaryParticle in DataModel

--- DataModel/Particle.h
+++ DataModel/Particle.h
@@ -137,13 +137,13 @@ class MCParticle : public Particle {
 	public:
 	
 	MCParticle() : Particle(0, 0., 0., Position(), Position(), 0., 0., Direction(), 0.,
-				tracktype::UNCONTAINED), ParticleID(0), ParentPdg(0), DirectParentID(0), StartsInFiducialVolume(false), TrackAngleX(0), TrackAngleY(0), TrackAngleFromBeam(0), EntersTank(false), TankEntryPoint(Position()), ExitsTank(false), TankExitPoint(Position()), TrackLengthInTank(0), EntersMrd(false), MrdEntryPoint(Position()), ExitsMrd(false), MrdExitPoint(Position()), PenetratesMrd(false), TrackLengthInMrd(0), MrdPenetration(0), MrdLayersPenetrated(0), MrdEnergyLoss(0), Flag(0), MCTriggerNum(0) {serialise=true;}
+				tracktype::UNCONTAINED), ParticleID(0), ParentPdg(0), PrimaryParentID(0), DirectParentID(0), StartsInFiducialVolume(false), TrackAngleX(0), TrackAngleY(0), TrackAngleFromBeam(0), EntersTank(false), TankEntryPoint(Position()), ExitsTank(false), TankExitPoint(Position()), TrackLengthInTank(0), EntersMrd(false), MrdEntryPoint(Position()), ExitsMrd(false), MrdExitPoint(Position()), PenetratesMrd(false), TrackLengthInMrd(0), MrdPenetration(0), MrdLayersPenetrated(0), MrdEnergyLoss(0), Flag(0), MCTriggerNum(0) {serialise=true;}
 	
 	MCParticle(int pdg, double sttE, double stpE, Position sttpos, Position stppos, 
 	  double sttt, double stpt, Direction startdir, double len, tracktype tracktypein,
-	  int partid, int parentpdg, int directparentid, int flagid, int triggernum)
+	  int partid, int parentpdg, int primaryparentid, int directparentid, int flagid, int triggernum)
 	: Particle(pdg, sttE, stpE, sttpos, stppos, sttt, stpt, startdir, len, tracktypein), 
-	  ParticleID(partid), ParentPdg(parentpdg), DirectParentID(directparentid), StartsInFiducialVolume(false), TrackAngleX(0), TrackAngleY(0), TrackAngleFromBeam(0), EntersTank(false), TankEntryPoint(Position()), ExitsTank(false), TankExitPoint(Position()), TrackLengthInTank(0), EntersMrd(false), MrdEntryPoint(Position()), ExitsMrd(false), MrdExitPoint(Position()), PenetratesMrd(false), TrackLengthInMrd(0), MrdPenetration(0), MrdLayersPenetrated(0), MrdEnergyLoss(0), Flag(flagid), MCTriggerNum(triggernum)
+	  ParticleID(partid), ParentPdg(parentpdg), PrimaryParentID(primaryparentid), DirectParentID(directparentid), StartsInFiducialVolume(false), TrackAngleX(0), TrackAngleY(0), TrackAngleFromBeam(0), EntersTank(false), TankEntryPoint(Position()), ExitsTank(false), TankExitPoint(Position()), TrackLengthInTank(0), EntersMrd(false), MrdEntryPoint(Position()), ExitsMrd(false), MrdExitPoint(Position()), PenetratesMrd(false), TrackLengthInMrd(0), MrdPenetration(0), MrdLayersPenetrated(0), MrdEnergyLoss(0), Flag(flagid), MCTriggerNum(triggernum)
 	  {
 		serialise=true;
 		// override Hit tracktype
@@ -160,6 +160,7 @@ class MCParticle : public Particle {
 	
 	inline int GetParticleID(){return ParticleID;}
 	inline int GetParentPdg(){return ParentPdg;}
+	inline int GetPrimaryParentID(){return PrimaryParentID;}
 	inline int GetDirectParentID(){return DirectParentID;}
 	inline int GetFlag(){return Flag;}
 	inline int GetMCTriggerNum(){return MCTriggerNum;}
@@ -189,6 +190,7 @@ class MCParticle : public Particle {
 	
 	inline void SetParticleID(int partidin){ParticleID=partidin;}
 	inline void SetParentPdg(int parentpdgin){ParentPdg=parentpdgin;}
+	inline void SetPrimaryParentID(int primaryparentidin){PrimaryParentID=primaryparentidin;}
 	inline void SetDirectParentID(int directparentidin){DirectParentID=directparentidin;}
 	inline void SetFlag(int flagidin){Flag=flagidin;}
 	inline void SetMCTriggerNum(int triggernumin){MCTriggerNum=triggernumin;}
@@ -274,6 +276,7 @@ class MCParticle : public Particle {
 	protected:
 	int ParticleID;
 	int ParentPdg;
+	int PrimaryParentID;
 	int DirectParentID;
 	int Flag;
 	int MCTriggerNum; // trigger window in which the particle was created
@@ -316,6 +319,7 @@ class MCParticle : public Particle {
 			
 			ar & ParticleID;
 			ar & ParentPdg;
+			ar & PrimaryParentID;
 			ar & DirectParentID;
 			ar & StartsInFiducialVolume;
 			
