0cfb918adb97d074bbd1eee7794f7549c2313492
dhavvval <ajana.dhaval.2@gmail.com>
2026-03-10 15:11:38 -0500

    small brain mistake

--- UserTools/PMTWaveformSim/PMTWaveformSim.cpp
+++ UserTools/PMTWaveformSim/PMTWaveformSim.cpp
@@ -156,6 +156,8 @@ bool PMTWaveformSim::Execute()
     // samples from hits that are close in time will be added together
     // key is hit time in clock ticks, value is amplitude
     std::map<uint16_t, uint16_t> sample_map;
+    std::map<uint16_t, std::vector<int>> hits_to_directparents_map;
+    std::map<uint16_t, std::vector<int>> hits_to_primaryparents_map;
 
     for (MCHit& mcHit : mcHits) {// Loop through each MCHit in the vector
       // skip negative hit times, what does that even mean if we're not using the smeared digit time?
